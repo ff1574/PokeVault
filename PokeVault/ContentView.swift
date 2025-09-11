b2ac38d4fdd5047ffe90a87d79bfff6b2befe6d8
ff1574 <ff1574@rit.edu>
2025-09-11 10:11:14 -0400

    Initial Commit

---
+++ PokeVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeVault
+//
+//  Created by Franko Fister (RIT Student) on 9/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeVault/PokeVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeVaultApp.swift
+//  PokeVault
+//
+//  Created by Franko Fister (RIT Student) on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
